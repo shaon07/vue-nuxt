da4cdd41482a933d627149c98d37b88026b2dab3
shaon07 <alishaon078@gmail.com>
2025-05-03 12:09:48 +0600

    [add] footer design

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,14 +4,13 @@
     <main class="content w-full">
       <slot></slot>
     </main>
-    <footer class="footer">
-      <h1>This is footer</h1>
-    </footer>
+    <Footer />
   </div>
 </template>
 
 <script setup>
 // no logic needed
+import Footer from "../components/shared/Footer.vue";
 import Header from "../components/shared/Header.vue";
 </script>
 
@@ -24,15 +23,6 @@ import Header from "../components/shared/Header.vue";
   .content {
     flex: 1;
     margin: 0 auto;
-  }
-
-  .footer {
-    background-color: #2d006e;
-    color: white;
-    padding: 2rem 1rem;
-    text-align: center;
-    font-size: 0.95rem;
-    line-height: 1.6;
   }
 }
 </style>
